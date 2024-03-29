918773f6acea97a049bc543a869beac5b388d5c4
calvarez-apex <74380252+calvarez-apex@users.noreply.github.com>
2024-03-29 04:05:50 -0600

    Add homeworks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstModuleModule } from './my-first-module/my-first-module.module';
+import { HomeworksModule } from './homeworks/homeworks.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MyFirstModuleModule } from './my-first-module/my-first-module.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MyFirstModuleModule
+    MyFirstModuleModule,
+    HomeworksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homeworks/homeworks.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeworksRoutingModule } from './homeworks-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeworksRoutingModule
+  ]
+})
+export class HomeworksModule { }
